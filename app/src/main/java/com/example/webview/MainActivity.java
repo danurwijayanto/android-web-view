a32f5f3178907fe0e3bce6ac318720dda3fd10f3
danurwijayanto <mobinity.fx@gmail.com>
2020-04-09 16:49:27 +0700

    add interestial add

--- app/src/main/java/com/example/webview/MainActivity.java
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.webview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 
@@ -11,9 +12,13 @@ import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.ads.InterstitialAd;
+
 
 public class MainActivity extends AppCompatActivity {
     private AdView mAdView;
+    private WebView webView;
+    private InterstitialAd mInterstitialAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,14 +32,31 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        WebView webView = (WebView)findViewById(R.id.webview);
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
+        mInterstitialAd.show();
+
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
+
+        webView = (WebView)findViewById(R.id.webview);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebChromeClient(new WebChromeClient());
         webView.loadUrl("file:///android_asset/www/index.html");
-//        webView.loadUrl("https://html5up.net/landed");
 
         mAdView = findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView != null && webView.canGoBack()){
+            webView.goBack();
+        }else{
+            finish();
+        }
     }
 }
